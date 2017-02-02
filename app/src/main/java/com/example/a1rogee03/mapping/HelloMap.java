c48bcc3f270559bdeeab16f2e0c1a7e9c201423c
ELRogers96 <elrogers96@gmail.com>
2017-02-02 10:57:52 +0000

    Part 4 Map Changes (( Default set to Litochoro, Greece )) Version 2

--- app/src/main/java/com/example/a1rogee03/mapping/HelloMap.java
+++ app/src/main/java/com/example/a1rogee03/mapping/HelloMap.java
@@ -6,11 +6,15 @@ import android.os.Bundle;
 import android.preference.PreferenceManager;
 
 import android.app.Activity;
-import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+
 import org.osmdroid.views.MapView;
 import org.osmdroid.util.GeoPoint;
 
-public class HelloMap extends Activity
+public class HelloMap extends Activity implements OnClickListener
 {
 
     MapView mv;
@@ -20,6 +24,9 @@ public class HelloMap extends Activity
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button SubmitButton = (Button) findViewById(R.id.SubmitButton);
+        SubmitButton.setOnClickListener(this);
 
         // this line tells OpenStreetMap about our app.
         // If you miss this out, you might get banned from OSM servers
@@ -31,5 +38,15 @@ public class HelloMap extends Activity
         mv.setBuiltInZoomControls(true);
         mv.getController().setZoom(14);
         mv.getController().setCenter(new GeoPoint(40.1,22.5));
+    }
+
+    @Override
+    public void onClick(View v) {
+        // Retrieve Latitude
+        EditText LatitudeEditText = (EditText) findViewById(R.id.LatitudeEditText);
+        double latitude = Double.parseDouble(LatitudeEditText.getText().toString());
+
+        EditText LongitudeEditText = (EditText) findViewById(R.id.LongitudeEditText);
+        double longitude = Double.parseDouble(LongitudeEditText.getText().toString());
     }
 }
