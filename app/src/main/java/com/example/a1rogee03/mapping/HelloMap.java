9ee0d235e83b40b5652e417af1bc1276f452247a
ELRogers96 <elrogers96@gmail.com>
2017-02-06 16:48:59 +0000

    Multiple Activities & Menus (Part 5)
    ((Exercises incomplete))

--- app/src/main/java/com/example/a1rogee03/mapping/HelloMap.java
+++ app/src/main/java/com/example/a1rogee03/mapping/HelloMap.java
@@ -2,10 +2,14 @@ package com.example.a1rogee03.mapping;
 
 import android.app.Activity;
 import org.osmdroid.config.Configuration;
+
+import android.content.Intent;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 
 import android.app.Activity;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
@@ -13,6 +17,13 @@ import android.widget.EditText;
 
 import org.osmdroid.views.MapView;
 import org.osmdroid.util.GeoPoint;
+
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.content.Intent;
+
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
 
 public class HelloMap extends Activity implements OnClickListener
 {
@@ -49,4 +60,50 @@ public class HelloMap extends Activity implements OnClickListener
         EditText LongitudeEditText = (EditText) findViewById(R.id.LongitudeEditText);
         double longitude = Double.parseDouble(LongitudeEditText.getText().toString());
     }
+
+
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        MenuInflater inflater=getMenuInflater();
+        inflater.inflate(R.menu.menu_hello_map, menu);
+        return true;
+    }
+
+
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        if(item.getItemId() == R.id.choosemap)
+        {
+            // react to the menu item being selected...
+            Intent intent = new Intent(this,MapChooseActivity.class);
+            startActivityForResult(intent,0);
+            return true;
+        }
+        return false;
+
+    }
+
+    protected void onActivityResult(int requestCode,int resultCode,Intent intent)
+    {
+
+        if(requestCode==0)
+        {
+
+            if (resultCode==RESULT_OK)
+            {
+                Bundle extras=intent.getExtras();
+                boolean cyclemap = extras.getBoolean("com.example.cyclemap");
+                if(cyclemap==true)
+                {
+                    mv.setTileSource(TileSourceFactory.CYCLEMAP);
+                }
+                else
+                {
+                    mv.getTileProvider().setTileSource(TileSourceFactory.MAPNIK);
+                }
+            }
+        }
+    }
+
+
 }
